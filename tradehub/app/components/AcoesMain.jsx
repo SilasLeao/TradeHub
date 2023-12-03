939b5747603d2571132154404a87984eb746eb9f
SilasLeao <leaosilas@gmail.com>
2023-12-03 15:58:40 -0300

    .

--- tradehub/app/components/AcoesMain.jsx
+++ tradehub/app/components/AcoesMain.jsx
@@ -1,11 +1,40 @@
 "use client";
+
+// 8QE9zJXLMnT7w6wppfyXEs
+
 import "./acoesMain.css";
+import { useEffect, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faMagnifyingGlass,
   faCaretDown,
 } from "@fortawesome/free-solid-svg-icons";
+
 export default function AcoesMain() {
+  const [codigoAcao, setCodigoAcao] = useState("");
+  function handleChange(event) {
+    setCodigoAcao(event.target.value);
+    console.log(codigoAcao);
+  }
+  function handleSearch() {
+    alert(codigoAcao);
+  }
+
+  // const [data, setData] = useState(null);
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     try {
+  //       const resposta = await fetch("https://www.exemplo.com/api/recurso");
+  //       const resultado = await resposta.json();
+  //       setData(resultado);
+  //     } catch (error) {
+  //       console.error("Erro ao buscar dados:", error);
+  //     }
+  //   };
+
+  //   fetchData();
+  // }, []);
+
   return (
     <>
       <div className="acoesMainContainer">
@@ -17,24 +46,32 @@ export default function AcoesMain() {
         <div className="searchContainer">
           <div className="searchHeader">
             <div className="searchBar">
-              <FontAwesomeIcon className="acoesIcon" icon={faMagnifyingGlass} />
-              <input type="text" placeholder="Pesquisar" />
+              <button onClick={handleSearch}>
+                <FontAwesomeIcon
+                  className="acoesIcon"
+                  icon={faMagnifyingGlass}
+                />
+              </button>
+              <input
+                onChange={handleChange}
+                type="text"
+                placeholder="Pesquisar"
+              />
             </div>
             <span>Filtrar por:</span>
-            <button>
+            <button className="filterButton">
               Valor
               <FontAwesomeIcon className="acoesIcon" icon={faCaretDown} />
             </button>
-            <button>
+            <button className="filterButton">
               Dividendos
               <FontAwesomeIcon className="acoesIcon" icon={faCaretDown} />
             </button>
-            <button>
+            <button className="filterButton">
               Crescimento
               <FontAwesomeIcon className="acoesIcon" icon={faCaretDown} />
             </button>
           </div>
-          <hr />
           <div className="cardContainer"></div>
         </div>
       </div>
