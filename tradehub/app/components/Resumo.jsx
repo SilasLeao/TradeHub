ded7c585a90d1a15f34067b28d86388d8b706592
SilasLeao <leaosilas@gmail.com>
2023-12-04 20:37:56 -0300

    .

--- tradehub/app/components/Resumo.jsx
+++ tradehub/app/components/Resumo.jsx
@@ -1,11 +1,39 @@
 "use client";
 import "./resumo.css";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 export default function Resumo() {
   return (
     <>
       <div className="resumoCard">
-        <p>resumo</p>
+        <div className="resumoTitle">
+          <span>Resumo</span>
+          <FontAwesomeIcon className="resumoIcon" icon={faArrowRight} />
+        </div>
+        <div className="conta">
+          <span>Conta</span>
+          <span>5GJV756PUC</span>
+        </div>
+        <div className="saldo">
+          <span>Saldo Atual</span>
+          <span className="resumoNumeroVerde">R$ 1.375,83</span>
+        </div>
+        <div className="totalInvestido">
+          <span>Total Investido</span>
+          <span>R$ 17.486,21</span>
+        </div>
+        <div className="valorAcumulado">
+          <span>Valor Acumulado</span>
+          <span className="resumoNumeroVerde">R$ 19.629,93</span>
+        </div>
+        <div className="valorAcumuladoPorcento">
+          <span>Valor Acumulado(%)</span>
+          <span className="resumoNumeroVerde">12,24%</span>
+        </div>
+        <div className="dividendos">
+          <span>Dividendos a Receber</span>
+          <span>R$ 53,27</span>
+        </div>
       </div>
     </>
   );
--- tradehub/app/components/TitleCard.jsx
+++ tradehub/app/components/TitleCard.jsx
@@ -5,7 +5,11 @@ export default function TitleCard() {
   return (
     <>
       <div className="titleCard">
-        <p>Titulo</p>
+        <p id="title">
+          Trade<span>Hub</span>
+        </p>
+        <p className="copyright">Copyright © 2023 TradeHub</p>
+        <p className="copyright">Todos os Direitos Reservados.</p>
       </div>
     </>
   );
