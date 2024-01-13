c1fa8a96bf9aade1014589c03a4c868a82f84abb
SilasLeão <leaosilas@gmail.com>
2024-01-13 13:49:23 -0300

    .

--- tradehub/app/components/AcoesMain.jsx
+++ tradehub/app/components/AcoesMain.jsx
@@ -64,52 +64,58 @@ export default function AcoesMain() {
       console.error("Erro ao buscar dados:", error);
     }
   };
+  const fetchAcaoDestaqueNegativa = async () => {
+    try {
+      const acaoDestaqueListaNegativa = await fetch(
+        "https://brapi.dev/api/quote/list?sortBy=change&sortOrder=asc&limit=4&token=8QE9zJXLMnT7w6wppfyXEs"
+      );
+      const lista = await acaoDestaqueListaNegativa.json();
+      return lista.stocks.map((stock) => ({
+        nome: stock.stock,
+        cotacao: stock.close,
+        variacao: stock.change,
+      }));
+    } catch (error) {
+      console.error("Erro ao buscar ações, ", error);
+    }
+  };
+  const fetchAcaoDestaquePositiva = async () => {
+    try {
+      const acaoDestaqueListaPositiva = await fetch(
+        "https://brapi.dev/api/quote/list?sortBy=change&sortOrder=desc&limit=4&token=8QE9zJXLMnT7w6wppfyXEs"
+      );
+      const lista = await acaoDestaqueListaPositiva.json();
+      return lista.stocks.map((stock) => ({
+        nome: stock.stock,
+        cotacao: stock.close,
+        variacao: stock.change,
+      }));
+    } catch (error) {
+      console.error("Erro ao buscar ações, ", error);
+    }
+  };
 
   useEffect(() => {
-    const fetchAcaoDestaqueNegativa = async () => {
+    const fetchData = async () => {
       try {
-        const acaoDestaqueListaNegativa = await fetch(
-          "https://brapi.dev/api/quote/list?sortBy=change&sortOrder=asc&limit=4&token=8QE9zJXLMnT7w6wppfyXEs"
-        );
-        const lista = await acaoDestaqueListaNegativa.json();
-        console.log("negativa", lista.stocks);
-        return lista.stocks.map((stock) => ({
-          nome: stock.stock,
-          cotacao: stock.close,
-          variacao: stock.change,
-        }));
+        const [negativas, positivas] = await Promise.all([
+          fetchAcaoDestaquePositiva(),
+          fetchAcaoDestaqueNegativa(),
+        ]);
+
+        if (negativas && positivas) {
+          setAcaoDestaque([...negativas, ...positivas]);
+        } else if (negativas) {
+          setAcaoDestaque([...negativas]);
+        } else if (positivas) {
+          setAcaoDestaque([...positivas]);
+        }
       } catch (error) {
-        console.error("Erro ao buscar ações, ", error);
-      }
-    };
-    const fetchAcaoDestaquePositiva = async () => {
-      try {
-        const acaoDestaqueListaPositiva = await fetch(
-          "https://brapi.dev/api/quote/list?sortBy=change&sortOrder=desc&limit=4&token=8QE9zJXLMnT7w6wppfyXEs"
-        );
-        const lista = await acaoDestaqueListaPositiva.json();
-        console.log("positiva", lista.stocks);
-        return lista.stocks.map((stock) => ({
-          nome: stock.stock,
-          cotacao: stock.close,
-          variacao: stock.change,
-        }));
-      } catch (error) {
-        console.error("Erro ao buscar ações, ", error);
+        console.error("Erro ao buscar dados, ", error);
       }
     };
 
-    Promise.all([fetchAcaoDestaqueNegativa(), fetchAcaoDestaquePositiva()])
-      .then(([negativas, positivas]) => {
-        setAcaoDestaque((prevCards) => [
-          ...prevCards,
-          ...(negativas ? [negativas] : []),
-          ...(positivas ? [positivas] : []),
-        ]);
-      })
-      .catch((error) => {
-        console.error("Erro ao buscar dados, ", error);
-      });
+    fetchData();
   }, []);
 
   let [chartTimeline, setChartTimeline] = useState(true);
