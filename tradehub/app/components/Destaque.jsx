4e5288b09c46352a35f31ec97cf8c96ac8bf489d
SilasLeão <leaosilas@gmail.com>
2024-01-14 09:53:54 -0300

    .

--- tradehub/app/components/Destaque.jsx
+++ tradehub/app/components/Destaque.jsx
@@ -6,8 +6,77 @@ import {
   faArrowDown,
   faArrowUp,
 } from "@fortawesome/free-solid-svg-icons";
+import { useEffect, useState } from "react";
 
 export default function Destaque() {
+  const [acaoDestaque, setAcaoDestaque] = useState([]);
+  const fetchAcaoDestaqueNegativa = async () => {
+    try {
+      const acaoDestaqueListaNegativa = await fetch(
+        "https://brapi.dev/api/quote/list?sortBy=change&sortOrder=asc&limit=4&token=8QE9zJXLMnT7w6wppfyXEs"
+      );
+      const lista = await acaoDestaqueListaNegativa.json();
+      return lista.stocks.map((stock) => ({
+        nome: stock.stock,
+        cotacao: stock.close,
+        variacao: stock.change,
+      }));
+    } catch (error) {
+      console.error("Erro ao buscar ações, ", error);
+    }
+  };
+  const fetchAcaoDestaquePositiva = async () => {
+    try {
+      const acaoDestaqueListaPositiva = await fetch(
+        "https://brapi.dev/api/quote/list?sortBy=change&sortOrder=desc&limit=4&token=8QE9zJXLMnT7w6wppfyXEs"
+      );
+      const lista = await acaoDestaqueListaPositiva.json();
+      return lista.stocks.map((stock) => ({
+        nome: stock.stock,
+        cotacao: stock.close,
+        variacao: stock.change,
+      }));
+    } catch (error) {
+      console.error("Erro ao buscar ações, ", error);
+    }
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const [negativas, positivas] = await Promise.all([
+          fetchAcaoDestaquePositiva(),
+          fetchAcaoDestaqueNegativa(),
+        ]);
+
+        if (negativas && positivas) {
+          const acoesIntercaladas = [];
+          for (
+            let i = 0;
+            i < Math.max(negativas.length, positivas.length);
+            i++
+          ) {
+            if (positivas[i]) {
+              acoesIntercaladas.push(positivas[i]);
+            }
+            if (negativas[i]) {
+              acoesIntercaladas.push(negativas[i]);
+            }
+          }
+
+          setAcaoDestaque(acoesIntercaladas);
+        } else if (negativas) {
+          setAcaoDestaque([...negativas]);
+        } else if (positivas) {
+          setAcaoDestaque([...positivas]);
+        }
+      } catch (error) {
+        console.error("Erro ao buscar dados, ", error);
+      }
+    };
+
+    fetchData();
+  }, []);
   return (
     <>
       <div className="destaqueCard">
---
+++ tradehub/app/components/DestaqueComponentInfo.jsx
@@ -0,0 +1,10 @@
+"use client";
+
+import "./destaqueComponentInfo.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowDown, faArrowUp } from "@fortawesome/free-solid-svg-icons";
+
+export default function DestaqueComponentInfo({ acao }) {
+  const nome = acao.nome;
+  const variacao = acao.variacao;
+}
