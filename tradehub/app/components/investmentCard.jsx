9f940d560f8429ee33e154dd698e1459c32189d8
SilasLeão <leaosilas@gmail.com>
2024-01-14 13:01:30 -0300

    .

--- tradehub/app/components/investmentCard.jsx
+++ tradehub/app/components/investmentCard.jsx
@@ -8,24 +8,40 @@ import {
 import classNames from "classnames";
 export default function InvestmentCard({ investimento }) {
   const nome = investimento.simbolo;
-  const valor = investimento.cotacao;
-  const variacao = investimento.variacao;
+  const valorAplicado = investimento.valor_aplicado;
   const quantidade = investimento.quantidade;
-  const corTextoClass = classNames({
-    vermelho: variacao < 0,
-    verde: variacao >= 0,
-  });
+  let rentabilidade = 0;
+  const fetchInvestmentData = async () => {
+    try {
+      const resposta = await fetch(
+        `https://brapi.dev/api/quote/${nome}?token=8QE9zJXLMnT7w6wppfyXEs`
+      );
+      const resultado = await resposta.json();
+      rentabilidade = resultado.results[0].regularMarketPrice * quantidade;
+      console.log(rentabilidade);
+    } catch (error) {
+      console.error("Erro ao buscar dados:", error);
+    }
+  };
+  fetchInvestmentData();
+
+  // const corTextoClass = classNames({
+  //   vermelho: variacao < 0,
+  //   verde: variacao >= 0,
+  // });
 
   return (
     <>
       <div className="investmentCard">
         <p className="investmentCardTitle">{nome}</p>
         <p className="investmentCardPrice">
-          R$ {Number(valor * quantidade).toFixed(2)}
+          {/* R$ {Number(valor * quantidade).toFixed(2)} */}
         </p>
         <hr className="investmentCardHr" />
-        <p className={`investmentCardChange ${corTextoClass}`}>
-          {Number(variacao).toFixed(2)}%
+        <p className={`investmentCardChange`}>
+          {" "}
+          {/* corTextoClass*/}
+          {/* {Number(variacao).toFixed(2)}% */}
         </p>
         <div className="investmentCardBtns">
           <FontAwesomeIcon className="investmentCardIcon" icon={faInfoCircle} />
