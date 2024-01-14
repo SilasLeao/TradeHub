8695314f04267d724a67d0ea0bbee0255fe72d78
SilasLeão <leaosilas@gmail.com>
2024-01-14 10:12:41 -0300

    .

--- tradehub/app/components/Destaque.jsx
+++ tradehub/app/components/Destaque.jsx
@@ -7,6 +7,7 @@ import {
   faArrowUp,
 } from "@fortawesome/free-solid-svg-icons";
 import { useEffect, useState } from "react";
+import DestaqueComponentInfo from "./DestaqueComponentInfo";
 
 export default function Destaque() {
   const [acaoDestaque, setAcaoDestaque] = useState([]);
@@ -77,6 +78,7 @@ export default function Destaque() {
 
     fetchData();
   }, []);
+
   return (
     <>
       <div className="destaqueCard">
@@ -84,37 +86,10 @@ export default function Destaque() {
           <span>Destaque</span>
           <FontAwesomeIcon className="destaqueTitleIcon" icon={faFilter} />
         </div>
-        <div className="destaqueVermelho">
-          <FontAwesomeIcon className="" icon={faArrowDown} />
-          <span>KLBN4 -5.97%</span>
-        </div>
-        <div className="destaqueVerde">
-          <FontAwesomeIcon className="" icon={faArrowUp} />
-          <span>CIEL3 7.96%</span>
-        </div>
-        <div className="destaqueVermelho">
-          <FontAwesomeIcon className="" icon={faArrowDown} />
-          <span>PETR4 -2.13%</span>
-        </div>
-        <div className="destaqueVerde">
-          <FontAwesomeIcon className="" icon={faArrowUp} />
-          <span>HCTR11 1.64%</span>
-        </div>
-        <div className="destaqueVermelho">
-          <FontAwesomeIcon className="" icon={faArrowDown} />
-          <span>GOGL34 -0.51%</span>
-        </div>
-        <div className="destaqueVerde">
-          <FontAwesomeIcon className="" icon={faArrowUp} />
-          <span>MXRF11 0.29%</span>
-        </div>
-        <div className="destaqueVermelho">
-          <FontAwesomeIcon className="" icon={faArrowDown} />
-          <span>ITSA4 -0.10%</span>
-        </div>
-        <div className="destaqueVerde">
-          <FontAwesomeIcon className="" icon={faArrowUp} />
-          <span>SPTW11 0.51%</span>
+        <div>
+          {acaoDestaque.map((acao, index) => (
+            <DestaqueComponentInfo key={index} acao={acao} />
+          ))}
         </div>
       </div>
     </>
--- tradehub/app/components/DestaqueComponentInfo.jsx
+++ tradehub/app/components/DestaqueComponentInfo.jsx
@@ -7,4 +7,15 @@ import { faArrowDown, faArrowUp } from "@fortawesome/free-solid-svg-icons";
 export default function DestaqueComponentInfo({ acao }) {
   const nome = acao.nome;
   const variacao = acao.variacao;
+
+  return (
+    <>
+      <div className={variacao < 0 ? "destaqueVermelho" : "destaqueVerde"}>
+        <FontAwesomeIcon icon={variacao < 0 ? faArrowDown : faArrowUp} />
+        <span>
+          {nome} {variacao.toFixed(2)}%
+        </span>
+      </div>
+    </>
+  );
 }
