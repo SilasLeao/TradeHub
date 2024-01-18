8ec827e0dbb73f73249e5dbd44f00bb8d4ca2aa5
SilasLeão <leaosilas@gmail.com>
2024-01-18 10:12:48 -0300

    fav button render

--- tradehub/app/components/AcaoCard.jsx
+++ tradehub/app/components/AcaoCard.jsx
@@ -1,5 +1,5 @@
 import "./acaoCard.css";
-import { useState, useContext } from "react";
+import { useState, useContext, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faInfo,
@@ -8,9 +8,12 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 import { faStar as faEmptyStar } from "@fortawesome/free-regular-svg-icons";
 import classNames from "classnames";
+import { createClient } from "@supabase/supabase-js";
 import CardInfo from "./cardInfo";
 import { InfoContainerContext } from "../mainPage/page";
 export default function AcaoCard({ acao }) {
+  const [watchlist, setWatchlist] = useState([]);
+
   const nome = acao.symbol;
   const marketCap = acao.marketCap;
   const fullName = acao.fullName;
@@ -21,6 +24,11 @@ export default function AcaoCard({ acao }) {
     acaoVerde: variacao >= 0,
   });
 
+  const supabaseUrl = "https://njjjjpkgxodlrhrysbev.supabase.co";
+  const supabaseKey =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5qampqcGtneG9kbHJocnlzYmV2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDE4MTg1MzQsImV4cCI6MjAxNzM5NDUzNH0.BJ8RAHt3jHIAJgq9vD1P8_gaWI-R-zn9AbGN71zyItc";
+  const supabase = createClient(supabaseUrl, supabaseKey);
+
   const infoContainerContext = useContext(InfoContainerContext);
 
   function handleInfoButton() {
@@ -29,6 +37,26 @@ export default function AcaoCard({ acao }) {
     sessionStorage.setItem("marketCap", marketCap);
     infoContainerContext.toggleInfoContainerStatus("acoes");
   }
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const { data, error } = await supabase
+          .from("Watchlist")
+          .select("*")
+          .filter("usuario_id", "eq", "5GJV756PUC");
+        setWatchlist(
+          data.map((investimento) => {
+            return investimento.simbolo;
+          })
+        );
+      } catch (error) {
+        console.error("Erro ao consultar watchlist", error);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   return (
     <>
@@ -48,7 +76,16 @@ export default function AcaoCard({ acao }) {
             onClick={handleInfoButton}
           />
           <button>Comprar</button>
-          <FontAwesomeIcon className="acaoCardIcon" icon={faEmptyStar} />
+          <FontAwesomeIcon
+            className="acaoCardIcon"
+            icon={
+              watchlist && watchlist.length > 0
+                ? watchlist.includes(nome)
+                  ? faStar
+                  : faEmptyStar
+                : ""
+            }
+          />
         </div>
       </div>
     </>
