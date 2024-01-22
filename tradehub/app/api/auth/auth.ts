343fc7036adf2b1fbd6db0fed8ee046a1e9906b7
SilasLeão <leaosilas@gmail.com>
2024-01-22 11:33:29 -0300

    .

---
+++ tradehub/app/api/auth/auth.ts
@@ -0,0 +1,6 @@
+import { NextAuthOptions, User, getServerSession } from "next-auth";
+import { useSession } from "next-auth/react";
+import { redirect } from "next/navigation";
+
+import CredentialsProvider from "next-auth/providers/credentials";
+import GoogleProvider from "next-auth/providers/google";
