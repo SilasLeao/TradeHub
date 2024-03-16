0d9b61db521c02199d82b6134aaa4a309cc3014f
SilasLeão <leaosilas@gmail.com>
2024-03-16 17:47:27 -0300

    .

--- tradehub/app/layout.js
+++ tradehub/app/layout.js
@@ -2,7 +2,7 @@
 
 import { SessionProvider } from "next-auth/react";
 
-const Layout = ({ children, pageProps, session }) => {
+const Layout = ({ children, session }) => {
   return (
     <SessionProvider session={session}>
       <html lang="en">
